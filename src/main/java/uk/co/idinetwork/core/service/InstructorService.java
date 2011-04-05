d45f80d5c4aa774da601d31470393ce1bd372ba9
Mark Hunter <arcotc@yahoo.co.uk>
2011-04-05 08:53:40 +0100

    Split from core

--- src/main/java/uk/co/idinetwork/core/service/InstructorService.java
+++
@@ -1,8 +0,0 @@
-package uk.co.idinetwork.core.service;
-
-import uk.co.idinetwork.core.model.Instructor;
-
-public interface InstructorService {
-	public Instructor findInstructor(String instructorId);
-	Instructor saveInstructor(String forename, String surname, String adiCode);
-}
